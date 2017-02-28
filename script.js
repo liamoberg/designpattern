97dfe03608bca2335a300b1af037128f14dfa9f9
Liam Öberg <Liam@LiamsMacbook.local>
2017-02-28 23:37:36 +0100

    Added alot of stuff, im bad at GitHub. Some functions, some buttons etc.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+// I've used the module pattern with object literals simply because I think it is easy to use and the code gets very structured.
+// I used the constructor pattern for my objects and it's the same there, a smooth and easy way to create objects.
+
 const movieDataBase = {
+    
+    // Array that will hold all the movies
     movies :[
 
     ],
 
+    // Constructor that contains all the prop in the objects we create
     Movie: class movie {
       constructor(name, year, genre, ratings) {
         this.name = name;
@@ -12,17 +18,20 @@ const movieDataBase = {
       }
     },
 
+    // Function that creates movies from the constructor above
     pushMovie: function() {
-        this.movies.push(new movieDataBase.Movie('Batman begins', 2005, ["Thriller", "Action"], [10, 6, 8, 9]))
-        this.movies.push(new movieDataBase.Movie('Langetslang', 2001, ["Drama", "Romance"], [2, 1, 3, 4]))
-        this.movies.push(new movieDataBase.Movie('Dåligfilm', 2005, ["Sci-fi", "Adventure"], [1, 2, 1, 3]))
-        this.movies.push(new movieDataBase.Movie('fifty shades of code', 2015, ["Drama", "Tilt"], [9, 8, 7, 9]))
-        this.movies.push(new movieDataBase.Movie('Langamanga', 2015, ["Comedy", "Tilt"], [10, 8, 7, 9]))
+        this.movies.push(new movieDataBase.Movie('Batman begins', 2005, ['Thriller', 'Action'], [10, 6, 8, 9]))
+        this.movies.push(new movieDataBase.Movie('Langetslang', 2001, ['Drama', 'Romance'], [2, 1, 3, 4]))
+        this.movies.push(new movieDataBase.Movie('Dåligfilm', 2005, ['Sci-fi', 'Adventure'], [1, 2, 1, 3]))
+        this.movies.push(new movieDataBase.Movie('fifty shades of code', 2015, ['Drama', 'Tilt'], [9, 8, 7, 9]))
+        this.movies.push(new movieDataBase.Movie('Langamanga', 2015, ['Comedy', 'Tilt'], [10, 8, 7, 9]))
     },
 
+    // Function that takes 2 parameters, the movie you want to rate and the rating
     rateMovie: function(movie, rating){
         const movies = this.movies;
-        
+        // loop that checks all movies and if the name you put in the parameter matches with a name of a movie,
+        // pushes the rating that you put in the parameter.
         for (let i = 0; i < movies.length; i++){
             if(movies[i].name === movie){
                 movies[i].ratings.push(rating);
@@ -30,6 +39,7 @@ const movieDataBase = {
         }
     },
 
+    // Function that takes a year as parameter and returns a new array with all the movies that matches the year
     getMoviesThisYear: function(year){
         return this.movies
         .filter(movie => {
@@ -37,6 +47,8 @@ const movieDataBase = {
         })
     },
 
+
+    // Function that takes a genre as parameter and for every movie that has the genre filters into a new array.
      getMovieByGenre: function(genre){
         return this.movies
         .filter(movie =>{
@@ -48,6 +60,8 @@ const movieDataBase = {
          })
     },
 
+    // Function that goes through movies rating and calculates averagerating and adds a new property with the average-
+    // rating to the object.
     getWithAverageRating: function(){
         this.movies
         .forEach(movie => {
@@ -59,6 +73,8 @@ const movieDataBase = {
         return this.movies;
     },
 
+
+    // function that checks all the averageratings of the movies and returns the one with worst rating.
     getWorstRating: function(){
         const movies = this.getWithAverageRating();
         return movies.reduce(function(previousValue, value){
@@ -66,6 +82,7 @@ const movieDataBase = {
         })
     },
     
+    // same as above but the highest rated.
     getHighestRating: function(){
         const movies = this.getWithAverageRating();
         return movies.reduce(function(previousValue, value){
@@ -76,36 +93,70 @@ const movieDataBase = {
 };
 
 
+
+const functionBase = {
+
+    // function that loops through all the movies in the database and adds them into the string variabel movies.
+    // after that I change the innerHTML of the div showMovies to the var movies.
+    displayAllMovies: function(){
+        const movie = movieDataBase.movies;
+        var movies = '';
+            for (let i = 0; i < movie.length; i++){
+                movies += `Title: ${movie[i].name} Release: ${movie[i].year} Genre: ${movie[i].genre} Ratings: ${movie[i].ratings}<br>`;
+            }   
+        document.getElementById('showMovies').innerHTML = movies;
+    },
+    
+    // function that logs all movies in the console
+    showAllMovies: function(){
+        console.log(movieDataBase.movies);
+    },
+
+    // function that saves the return of the function getWorstRating into a const that is calle movie.
+    // Then changes the innerHTML to the const movies different attributes
+    showWorstRatedMovie: function(){
+        const movie = movieDataBase.getWorstRating();
+            document.getElementById('showMovies').innerHTML = `Title: ${movie.name} Release: ${movie.year} Genre: ${movie.genre} Averagerating: ${movie.averageRatings}`; 
+    },
+
+    // same as above but the highestrated
+    showHighestRatedMovie: function(){
+        const movie = movieDataBase.getHighestRating();
+            document.getElementById('showMovies').innerHTML = `Title: ${movie.name} Release: ${movie.year} Genre: ${movie.genre} Averagerating: ${movie.averageRatings}`; 
+    }
+};
+
+
 movieDataBase.pushMovie();
 movieDataBase.rateMovie('Batman begins', 4);
 console.log(movieDataBase.getMovieByGenre(['Tilt', 'Drama']));
 console.log(movieDataBase.getMoviesThisYear(2005));
 console.log(movieDataBase.getWorstRating());
 console.log(movieDataBase.getHighestRating());
-console.log(movieDataBase.movies);
 
-//     Movie: function(name, year, genre, ratings){
-//         this.name = name;
-//         this.year = year;
-//         this.genre = genre;
-//         this.ratings = ratings;
+// all my buttons in one namespace with the eventlisteners etc
+const eventSpace = {
+    displayAll: function(){
+        document.getElementById('allmovies').addEventListener('click', functionBase.showAllMovies);
+        document.getElementById('allmovies').addEventListener('click', functionBase.displayAllMovies);
+    },
 
-//         this.showMovie = function(){
-//             return this.name + ' ' + this.year + ' ' + this.genre + ' ' + this.ratings;
-//         }
-//     }
-    
-    
-//     // showAllMovies: function(){
-//     //     console.log(this.movies);
-//     // },
-//     // addMovie: function(name, year, genre){
-//     //     this.name = name;
-//     //     this.year = year;
-//     //     this.genre = genre;
-//     //     return this.movies
-//     // }
+    displayWorst: function(){
+        document.getElementById('worstMovie').addEventListener('click', functionBase.showWorstRatedMovie);
+    },
 
+    displayBest: function(){
+        document.getElementById('bestMovie').addEventListener('click', functionBase.showHighestRatedMovie);
+    }
+
+
+};
+
+
+
+eventSpace.displayAll();
+eventSpace.displayWorst();
+eventSpace.displayBest();
 
 
 
